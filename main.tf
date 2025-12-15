d3a49e7b369afa27767a3644292cfe5b3b0cc738
javjm <javjm@outlook.com>
2025-12-15 18:23:25 +0000

    Update HCP terraform org

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "JTestOrg"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "pc-learn-terraform"
     }
   }
 }
